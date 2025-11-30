a063d0a3a2f966c937183e2f6bcd83d2fea01a61
zehramans <zehramansirova0@gmail.com>
2025-11-30 08:15:34 -0500

    changes saved

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""nginx logs statistics"""
+from pymongo import MongoClient
+
+def count(coll, method):
+    """count da """
+    res = coll.count_documents({"method": method})
+    return res
+
+if __name__ = "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    print(f"{nginx_collection.count_documents({})} logs")
+    print("Methods:")
+    for i in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(f"	method {i}: {count(nginx_collection, i)}")
+    print("{} status check".format(nginx_collection.count_documents({"path": "/status"})))
